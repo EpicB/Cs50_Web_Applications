8f7059894a6366dbe60f0df53da0080112af42b8
EpicB <git@github.com:EpicB/lecture0.git>
2020-01-14 03:03:42 +0200

    a
    Signed-off-by: EpicB <git@github.com:EpicB/lecture0.git>

--- Cs50/lecture2/seasionsflask/applications.py
+++ Cs50/lecture2/seasionsflask/applications.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"]= "filesystem"
 Session(app)
-app.config.from_object(__name__)
 
 Session["note"]=[]
 @app.route("/", methods=["POST","GET"])
