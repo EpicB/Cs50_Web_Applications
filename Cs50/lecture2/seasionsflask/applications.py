d892546791ae29b8e4a93cbafa494f6055a3559f
EpicB <git@github.com:EpicB/lecture0.git>
2020-01-14 02:59:54 +0200

    a
    Signed-off-by: EpicB <git@github.com:EpicB/lecture0.git>

--- Cs50/lecture2/seasionsflask/applications.py
+++ Cs50/lecture2/seasionsflask/applications.py
@@ -6,13 +6,12 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"]= "filesystem"
 Session(app)
-notes = []
-
+Session.["note"]=[]
 @app.route("/", methods=["POST","GET"])
 def hello():
     if request.method == "POST":
         note=request.form.get("note")
-        notes.append(note)
-        return render_template("main.html",notes=notes)
+        Session.["note"].append(note)
+        return render_template("main.html",notes=Session.["note"])
     else:
         return render_template("main.html")
